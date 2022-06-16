5db0eafd37e4f2f3a0024f1a4403d928c1c66373
gaurab <81964995+gaurab-hacked@users.noreply.github.com>
2022-06-16 10:48:44 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// let toggleBtn = document.getElementById("toggle");
+// let list = document.getElementById("list");
+
+// toggleBtn.addEventListener('click',()=>{
+//     list.classList.toggle('class1'); 
+// });
+
+// let subForm = document.getElementById('subForm');
+// subForm.addEventListener('click',()=>{
+//     return 0;
+// })
+
+
+
+function clean(){
+    // console.log("good");
+    return 0;
+}
+
+
+
+
